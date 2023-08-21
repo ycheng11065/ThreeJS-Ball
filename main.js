0e7f07eb01f5be47f8617bcf607a2c505d5c97d9
Yu Cheng Li <ychengli11065@gmail.com>
2023-08-20 18:32:29 -0700

    sphere can now move

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import './style.css';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Scene
 const scene = new THREE.Scene();
@@ -7,7 +8,7 @@ const scene = new THREE.Scene();
 // Create Sphere
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
-  color: '#00ff83',
+  color: '#00FFFC',
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -21,7 +22,7 @@ scene.add(light);
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
-}
+};
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height);
@@ -34,6 +35,9 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 // Resize
 window.addEventListener('resize', () => {
@@ -43,5 +47,14 @@ window.addEventListener('resize', () => {
 
   // Update Camera
   camera.aspect = sizes.width / sizes.height;
-  renderer.setSize(sizes.width, sizes.height)
-})
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  controls.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+
+loop();
